51a8804b28f743bfd6c328c2dcc9dfd146919d70
Digvijay <panwardigvijay17@gmail.com>
2023-02-04 01:59:19 +0530

    product grid css changes

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
     }, [dispatch]);
 
     return (
-        <div className="App">
+        <div className="app">
             <Products />
         </div>
     );
--- src/components/product-card/ProductCard.tsx
+++ src/components/product-card/ProductCard.tsx
@@ -7,17 +7,19 @@ interface ProductCardProps {
 export function ProductCard({ product }: ProductCardProps) {
     const { title, price, image } = product;
     return (
-        <div className="product-card">
-            <div className="product-name" title={title}>
-                <p className="product-name-title">{title}</p>
-            </div>
-            <div className="product-price">
-                <p className="product-price-text">Price</p>
-                <p className="product-price-currency">$</p>
-                <p className="product-price-value">{price}</p>
-            </div>
-            <div className="product-image">
-                <img src={image} alt={title.substring(0, 10) + '...'} />
+        <div className="product-card-container">
+            <div className="product-card">
+                <div className="product-name" title={title}>
+                    <p className="product-name-title">{title}</p>
+                </div>
+                <div className="product-price">
+                    <p className="product-price-text">Price</p>
+                    <p className="product-price-currency">$</p>
+                    <p className="product-price-value">{price}</p>
+                </div>
+                <div className="product-image">
+                    <img src={image} alt={title.substring(0, 10) + '...'} />
+                </div>
             </div>
         </div>
     );
